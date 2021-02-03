c66df5c1178e022cfdb2e8f879155cc32d849693
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-03 22:06:31 +0530

    sub no argu with return type

---
+++ sub0302/src/sub0302.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : sub0302.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int sub() {
+	int x = 10, y = 5;
+	int c = x - y;
+	return (c);
+}
+int main() {
+	int c = sub();
+	cout << "The sub is : " << c << endl;
+}
