272c087861a2675a19347183d1eeda9a2d61aa04
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-03 22:20:54 +0530

    Multiply no argu with return type

---
+++ Multiply/src/Multiply.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Multiply.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int mul() {
+	int x=20,y=20;
+	int c = x * y;
+	return (c);
+}
+int main() {
+	int m = mul();
+	cout << "The multiplication answer is : " << m << endl;
+	return 0;
+}
