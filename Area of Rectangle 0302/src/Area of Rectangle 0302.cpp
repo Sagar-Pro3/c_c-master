1467b950bee166f781dcaae45afdbca186e356d4
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-03 23:24:20 +0530

    Area of Rectangle no argu , with return type

---
+++ Area of Rectangle 0302/src/Area of Rectangle 0302.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Area.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int rectangle() {
+	int l = 500, b = 300, r;
+	r = l * b;
+	return (r);
+}
+int main() {
+	int z = rectangle();
+	cout << "Area of Rectangle : " << z << endl; // prints !!!Hello World!!!
+	return 0;
+}
