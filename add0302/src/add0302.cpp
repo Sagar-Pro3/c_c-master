4db74ca786586ed78ae05e78585c41bddcd1d567
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-03 21:27:41 +0530

    add no argument with return type

---
+++ add0302/src/add0302.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : add0302.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int add() {
+	int a = 2, b = 2;
+	int c = a + b;
+	return (c);
+}
+int main() {
+	int r = add();
+	cout << "The value of r is : " << r << endl;
+}
+
