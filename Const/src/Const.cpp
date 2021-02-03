8f3f5b424814cafdaffb0c8053b1461eed2b73ef
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-03 18:12:36 +0530

    Const Program

---
+++ Const/src/Const.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Const.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int a = 10;
+	//a=3;
+	cout << "The value of a is : " << a << endl;
+	return 0;
+}
