ceeb47d181ee665e30b03075190baaef8b40a584
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-06 13:32:30 +0530

    Default Constructor

---
+++ Default Constructor/src/Default Constructor.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Default Constructor.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+class one {
+public:
+	one() {
+		cout<<"Hello"<<endl;
+	}
+};
+int main() {
+	one o1,o2,o3;
+	return 0;
+}
