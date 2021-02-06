401c1e33a014dc7da4933730f30ac3a5de3fb5a7
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-06 19:11:32 +0530

    Update Static data members.cpp

--- Static data members/src/Static data members.cpp
+++ Static data members/src/Static data members.cpp
@@ -7,39 +7,38 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
-class student {
-private:
-	int roll_no;
-	char name[50];
-	int marks;
+
+class demo {
+
+	static int x;
+
 public:
-	static int count;
-	student() {
-		count++;
+
+	void function1() {
+		x = x + 1;
 	}
 
-	void getdata() {
-		cout << "Enter student roll_no : " << endl;
-		cin >> roll_no;
-		cout << "Enter student name : " << endl;
-		cin >> name;
-		cout << "Enter student marks : " << endl;
-		cin >> marks;
+	void function2() {
+		cout << "x = " << x << endl;
 	}
-	void setdata() {
-		cout << "Enter student roll_no : " << roll_no << endl;
-		cout << "Enter student name : " << name << endl;
-		cout << "Enter student marks : " << marks << endl;
-	}
+
 };
-int student::count = 0;
-int main(void) {
-	student s1, s2;
-	s1.getdata();
-	s1.setdata();
-	s2.getdata();
-	s2.setdata();
-	cout << "Total student is : " << student::count;
+
+int demo::x;
+
+int main() {
+	demo obj1;
+
+	obj1.function2();
+
+
+	obj1.function1();
+
+
+	obj1.function2();
+
+
 	return 0;
 }
