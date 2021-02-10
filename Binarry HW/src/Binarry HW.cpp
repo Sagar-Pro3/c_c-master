36cf15e163d701c91b07a419fdad8048eacc7f87
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-10 12:41:29 +0530

    binary hw

--- Binarry HW/src/Binarry HW.cpp
+++ Binarry HW/src/Binarry HW.cpp
@@ -4,8 +4,6 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 class Binarry_hw {
@@ -15,7 +13,7 @@ public:
 		name1 = x;
 		name2 = y;
 	}
-	void operator-(Binarry_hw cb2) {
+	void operator +(Binarry_hw cb2) {
 		string name3 = name1 + cb2.name1;
 		string name4 = name2 + cb2.name2;
 		cout << name3 << name4;
@@ -23,13 +21,13 @@ public:
 	void dis() {
 		cout << name1 << name2;
 	}
-
 };
 int main() {
 	Binarry_hw b1, b2;
-	b1.set("Ram", "Sham");
-	b2.set("Changu" ,"Mangu");
-	b1.dis();
-	b2.dis();
+	b1.set("Ram", "Changu");
+	b2.set("Sham", "Mangu");
+	b1+b2;
+	/*b1.dis();
+	 b2.dis();*/
 	return 0;
 }
