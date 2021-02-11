aa8306cec6954209f9796789abd11232e038a802
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-11 08:44:29 +0530

    Pattern 10

---
+++ MyPattern 10/src/MyPattern 10.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : MyPattern.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+class name {
+public:
+	int line = 4;
+	int starcount = 1;
+	void fun() {
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= starcount; j++) {
+				cout << "*";
+
+			}
+			cout << endl;
+			starcount++;
+		}
+	}
+};
+int main() {
+	name n1;
+	n1.fun();
+	return 0;
+}
