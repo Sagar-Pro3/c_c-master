0a5c9bfa7a812428daf972c2e428a91cf96ec3c0
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-15 10:25:27 +0530

    StarPattern2

---
+++ StarPattern2/src/StarPattern2.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : StarPattern2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//*
+//**
+//***
+//****
+//*****
+
+#include <iostream>
+using namespace std;
+class name {
+public:
+	int line = 5;
+	int starcount = 1;
+	void fun() {
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= starcount; j++) {
+				cout << "*";
+			}
+			cout<<endl;
+			starcount++;
+		}
+	}
+
+private:
+};
+int main() {
+	name n1;
+	n1.fun();
+	cout << "" << endl; // prints 
+	return 0;
+}
