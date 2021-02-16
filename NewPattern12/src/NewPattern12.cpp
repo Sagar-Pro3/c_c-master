c8b3004955ae498a2586daef294894f06da6e1bf
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-16 22:16:17 +0530

    NewPattern12
      1
     22
    333

---
+++ NewPattern12/src/NewPattern12.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : NewPattern12.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//  1
+// 22
+//333
+
+#include <iostream>
+using namespace std;
+class name {
+public:
+	int line = 3;
+	int space = 2;
+	int numcount = 1;
+	int num = 1;
+	void fun() {
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= space; j++) {
+				cout << " ";
+			}
+			for (int k = 1; k <= numcount; k++) {
+				cout << num;
+			}
+			cout << endl;
+			num++;
+			numcount++;
+			space--;
+		}
+	}
+private:
+};
+int main() {
+	name n1;
+	n1.fun();
+	cout << "" << endl; // prints 
+	return 0;
+}
