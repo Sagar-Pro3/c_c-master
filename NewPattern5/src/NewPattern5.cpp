b2f0f57ce8b1b8bc10bad2389fb14ae341cc55ae
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-16 23:56:51 +0530

    NewPattern5
       A 
      B B 
     C C C 
    D D D D

---
+++ NewPattern5/src/NewPattern5.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : NewPattern5.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//   A
+//  B B
+// C C C
+//D D D D
+
+#include <iostream>
+using namespace std;
+class name {
+public:
+	int line = 4;
+	int space = 3;
+	int charcount = 1;
+	char value = 'A';
+	void fun() {
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= space; j++) {
+				cout << " ";
+			}
+			for (int k = 1; k <= charcount; k++) {
+				cout << value<<" ";
+			}
+			cout << endl;
+
+			charcount += 1;
+			space -= 1;
+			value++;
+
+		}
+	}
+};
+
+int main() {
+	name n1;
+	n1.fun();
+	cout << "" << endl; // prints 
+	return 0;
+}
