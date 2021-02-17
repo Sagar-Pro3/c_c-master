dd1c375e3e8bd17043f8f033d3aeebb661db219f
Sagar-Pro3 <78415093+Sagar-Pro3@users.noreply.github.com>
2021-02-17 23:21:51 +0530

    WednesdayPattern13
    * 
    * * 
    * * * 
    * * * * 
    * * * 
    * * 
    *

---
+++ WednesdayPattern13/src/WednesdayPattern13.cpp
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : WednesdayPattern13.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//*
+//* *
+//* * *
+//* * * *
+//* * *
+//* *
+//*
+
+#include <iostream>
+using namespace std;
+class name {
+public:
+	int line = 7;
+	int starcount = 1;
+	//int star = 1;
+	int mid = ((line + 1) / 2);
+	void fun() {
+		for (int i = 1; i <= line; i++) {
+			for (int j = 1; j <= starcount; j++) {
+				cout << "* ";
+			}
+			cout<<endl;
+			if (i < mid) {
+				//space--;
+				starcount++;
+			} else {
+				//space++;
+				starcount--;
+
+			}
+		}
+	}
+
+private:
+};
+int main() {
+	name n1;
+	n1.fun();
+	cout << "" << endl; // prints 
+	return 0;
+}
